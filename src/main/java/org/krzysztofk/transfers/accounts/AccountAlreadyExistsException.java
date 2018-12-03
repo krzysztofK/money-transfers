d80ef0f4ea41d8b0e753eab46e74e32755d4c3cf
k.wilaszek <k.wilaszek@ocado.com>
2018-12-03 12:21:32 +0100

    Access modifier changed

--- src/main/java/org/krzysztofk/transfers/accounts/AccountAlreadyExistsException.java
+++ src/main/java/org/krzysztofk/transfers/accounts/AccountAlreadyExistsException.java
@@ -1,4 +1,4 @@
 package org.krzysztofk.transfers.accounts;
 
-public class AccountAlreadyExistsException extends RuntimeException {
+class AccountAlreadyExistsException extends RuntimeException {
 }
